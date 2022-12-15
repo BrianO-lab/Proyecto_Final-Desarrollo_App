87dff8f70fc87c19e58f9794fec5a2cc08ee494e
Brian Ore <orebrian95@gmail.com>
2022-12-15 16:28:54 -0300

    creacion de screens

---
+++ src/screens/MainScreen.js
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MainScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+
+    </View>
+  )
+}
+
+export default MainScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
